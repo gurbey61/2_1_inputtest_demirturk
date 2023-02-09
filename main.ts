5f0b580c90007e7d889225464b859359f83a436b
GURBEY DEMIRTURK <gurbey61@gmail.com>
2023-02-08 21:46:29 -0600

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,19 @@
+basic.forever(function () {
+    if (input.buttonIsPressed(Button.A)) {
+        basic.showLeds(`
+            . # . # .
+            . # . # .
+            # . . . #
+            . # . # .
+            . . # . .
+            `)
+    } else {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            # # # # #
+            . . . . .
+            . . . . .
+            `)
+    }
+})
